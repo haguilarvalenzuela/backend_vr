951c57c22bdc71566a05206c7c338dd2a14526c6
Hardy <h.aguilarvalenzuela@gmail.com>
2019-04-02 15:47:00 -0300

    BACK UPDATE: actualizado codigo

--- tests/test_profesores.py
+++ tests/test_profesores.py
@@ -3,11 +3,11 @@ import tempfile
 
 import pytest
 
-from flaskr import api
+#from flaskr import api
 from models.profesor import Profesor
 
 
-@pytest.fixture
+#@pytest.fixture
 def client():
     db_fd, api.app.config['DATABASE'] = tempfile.mkstemp()
     api.app.config['TESTING'] = True
