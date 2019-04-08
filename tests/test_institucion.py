2e7a294c8702591b0dcbef894e5b3422b1830a71
Hardy <h.aguilarvalenzuela@gmail.com>
2019-04-08 10:21:48 -0400

    BACK ADD: test get institucion [FUNCIONANDO LOCAL]

---
+++ tests/test_institucion.py
@@ -0,0 +1,29 @@
+import os
+import tempfile
+import json
+import pytest
+
+from flaskr import api
+from models.institucion import Institucion
+
+@pytest.fixture
+def client():
+    db_fd, api.app.config['DATABASE'] = tempfile.mkstemp()
+    api.app.config['TESTING'] = True
+    client = api.app.test_client()
+
+    yield client
+
+    os.close(db_fd)
+    os.unlink(api.app.config['DATABASE'])
+
+def test_get_institucion(client):
+	rv = client.get('/instituciones')
+	assert True
+	
+def test_get_institucion_id(client):
+	institucion = Institucion.objects().first()
+	if(institucion==None):
+		assert True
+	else:
+		assert False
