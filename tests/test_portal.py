dd14c35326e2a2328add1ab07ece84ae67c4399b
Hardy <h.aguilarvalenzuela@gmail.com>
2019-04-08 10:29:57 -0400

    BACK ADD: test get portal [FUNCIONANDO LOCAL]

---
+++ tests/test_portal.py
@@ -0,0 +1,29 @@
+import os
+import tempfile
+import json
+import pytest
+
+from flaskr import api
+from models.portal import Portal
+
+@pytest.fixture
+def client():
+    db_fd, api.app.config['DATABASE'] = tempfile.mkstemp()
+    api.app.config['TESTING'] = True
+    client = api.app.test_client()
+
+    yield client
+
+    os.close(db_fd)
+    os.unlink(api.app.config['DATABASE'])
+
+def test_get_portal(client):
+	rv = client.get('/portales')
+	assert True
+
+def test_get_portal_id(client):
+	portal = Portal.objects().first()
+	if(portal==None):
+		assert True
+	else:
+		assert False
