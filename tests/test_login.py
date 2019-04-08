2457470046bd52a154c5efaf8cfa61ae272575b4
Hardy <h.aguilarvalenzuela@gmail.com>
2019-04-08 10:27:01 -0400

    BACK ADD: test post loging/logout [FUNCIONANDO LOCAL]

---
+++ tests/test_login.py
@@ -0,0 +1,33 @@
+import os
+import tempfile
+import json
+import pytest
+
+from flaskr import api
+
+@pytest.fixture
+def client():
+    db_fd, api.app.config['DATABASE'] = tempfile.mkstemp()
+    api.app.config['TESTING'] = True
+    client = api.app.test_client()
+
+    yield client
+
+    os.close(db_fd)
+    os.unlink(api.app.config['DATABASE'])
+
+def test_post_login(client):
+	data = {
+		'tipo': 'ADMINISTRADOR',
+		'email': 'prueba@prueba.prueba',
+		'password': 'asd'
+	}
+
+	data = json.dumps(data)
+	data = data.encode()
+	rv = client.post('/login', data=data)
+	assert True
+
+def test_post_logout(client):
+	rv = client.post('/logout')
+	assert True
