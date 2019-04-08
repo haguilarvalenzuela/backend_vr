99462659d9f8a8801e000f8babe234c922577762
Hardy <h.aguilarvalenzuela@gmail.com>
2019-04-08 10:14:00 -0400

    BACK ADD: test get grado [FUNCIONANDO LOCAL]

---
+++ resources/test_grado.py
@@ -0,0 +1,29 @@
+import os
+import tempfile
+import json
+import pytest
+
+from flaskr import api
+from models.grado import Grado
+
+@pytest.fixture
+def client():
+    db_fd, api.app.config['DATABASE'] = tempfile.mkstemp()
+    api.app.config['TESTING'] = True
+    client = api.app.test_client()
+
+    yield client
+
+    os.close(db_fd)
+    os.unlink(api.app.config['DATABASE'])
+
+def test_get_grado(client):
+	rv = client.get('/grados')
+	assert True
+
+def test_get_grado_id(client):
+	grado = Grado.objects().first()
+	if(grado==None):
+		assert True
+	else:
+		assert False
