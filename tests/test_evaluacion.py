14a12fc62f8f7174eb0860600044227e8e8d55c6
Hardy <h.aguilarvalenzuela@gmail.com>
2019-04-08 10:08:56 -0400

    BACKK ADD: test get evaluacion [FUNCIONANDO LOCAL]

---
+++ tests/test_evaluacion.py
@@ -0,0 +1,30 @@
+import os
+import tempfile
+import json
+import pytest
+
+from flaskr import api
+from models.evaluacion import Evaluacion
+
+@pytest.fixture
+def client():
+    db_fd, api.app.config['DATABASE'] = tempfile.mkstemp()
+    api.app.config['TESTING'] = True
+    client = api.app.test_client()
+
+    yield client
+
+    os.close(db_fd)
+    os.unlink(api.app.config['DATABASE'])
+
+def test_get_evaluaciones(client):
+	rv = client.get('/evaluaciones')
+	assert True
+
+def test_get_evaluacion_id(client):
+	evaluacion = Evaluacion.objects().first()
+	if(evaluacion==None):
+		assert True
+	else:
+		assert False
+
