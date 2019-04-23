e64d3074ac5387c2b387666f6600ce7c125e7c13
Hardy <h.aguilarvalenzuela@gmail.com>
2019-04-23 11:02:09 -0400

    BACK UPDATE: modelo grado

--- models/grado.py
+++ models/grado.py
@@ -6,9 +6,18 @@ import mongoengine_goodjson as gj
 class Grado(gj.Document):
     institucion = db.ReferenceField(Institucion)
     nivel = db.IntField()
-    identificador = db.StringField(max_length=60)
+    identificador = db.StringField(max_length=5)
+    activo = db.BooleanField(default=True)
     def __str__(self):
         return str(self.nivel)+"°"+self.identificador
 
     def getGrado(self):
         return str(self.nivel)+"°"+self.identificador
+    
+    def to_dict(self):
+        return {
+            "id": str(self.id),
+            "nivel": self.nivel,
+            "identificador": self.identificador,
+            "activo": self.activo
+        }
