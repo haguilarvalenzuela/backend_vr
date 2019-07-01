6bf22da9439e16a0517839a01c9c302258bdd13e
haguilarvalenzuela <haguilar@nice.cl>
2019-07-01 19:12:15 -0400

    fixing imgs path

--- tests/test_alumno2.py
+++ tests/test_alumno2.py
@@ -190,7 +190,6 @@ def test_get_alumno_imagen(client):
     if alumno == None:
         assert True
     else:
-        test_post_alumno_imagen(client)
         rv = client.get('/alumno/imagen/'+str(alumno.id))
         if rv._status_code == 200:
             assert True
