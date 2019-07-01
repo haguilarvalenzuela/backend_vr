b0f1e28e8c5b7d3f37afdd24c5d4b4f10546f8ff
haguilarvalenzuela <haguilar@nice.cl>
2019-07-01 16:47:42 -0400

    path fix

--- tests/test_alumno2.py
+++ tests/test_alumno2.py
@@ -162,8 +162,8 @@ def test_delete_alumno_recurso(client):
 def test_post_alumno_imagen(client):
     
     with api.app.app_context():
-        path_img = os.path.join(api.app.root_path, "uploads/categorias/default.jpg")
-        print(path_img)
+        path_img = os.path.join(os.getcwd(), "../flaskr/uploads/categorias/default.jpg")
+        print(os.getcwd())
         with open(path_img, 'rb') as img_open:
             img = BytesIO(img_open.read())
             
