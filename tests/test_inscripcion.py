fd38bbe2fde0bc7f0b1d37fd3c52edfed8c5a7a3
haguilarvalenzuela <haguilar@nice.cl>
2019-07-02 13:59:23 -0400

    tests inscripcion

--- tests/test_inscripcion.py
+++ tests/test_inscripcion.py
@@ -20,70 +20,97 @@ def client():
     os.close(db_fd)
     os.unlink(api.app.config['DATABASE'])
 
-def test_get_inscripcion(client):
-	rv = client.get('/inscripciones')
-	assert True
-
-def test_get_inscripcion_id(client):
+def test_get_inscripcion_item(client):
 	inscripcion = Inscripcion.objects().first()
-	if(inscripcion==None):
+	if inscripcion == None:
 		assert True
 	else:
 		rv = client.get('/inscripciones/'+str(inscripcion.id))
-		assert True
+		if rv._status_code == 200:
+			assert True
+		else:
+			assert False
 
-def test_get_inscripcion_curso(client):
-	curso = Curso.objects().first()
-	if(curso==None):
-		assert True
-	else:
-		rv = client.get('/inscripciones_curso/'+str(curso.id))
-		assert True
-
-def test_get_inscripcion_alumnos(client):
-	alumno = Alumno.objects().first()
-	if(alumno==None):
-		assert True
-	else:
-		rv = client.get('/inscripciones_alumno/'+str(alumno.id))
-		assert True
-
-def test_post_inscripcion_curso(client):
-	curso = Curso.objects().first()
-	alumno = Alumno.objects().first()
-	if((curso==None) or (alumno==None)):
-		assert True
-	else:
-		data = {
-			"alumno": str(alumno.id)
-		}
-		rv = client.get('/inscripciones_curso/'+str(curso.id), data=data)
-		assert True
-
-def test_put_inscripcion_id(client):
+def test_delete_inscripcion_item(client):
 	inscripcion = Inscripcion.objects().first()
-	alumno = Alumno.objects().first()
-	curso = Curso.objects().first()
-	if((inscripcion==None) or (alumno==None) or (curso==None)):
-		assert True
-	else:
-		historial = Historial()
-		historial.data = "Testing"
-		data = {
-			"alumno": str(alumno.id),
-			"curso": str(curso.id),
-			"estado": "ENVIADA",
-			"historial": [historial]
-		}
-
-		rv = client.put('/inscripciones/'+str(inscripcion.id), data=data)
-		assert True
-
-def test_delete_inscripcion_id(client):
-	inscripcion = Inscripcion.objects().first()
-	if(inscripcion==None):
+	if inscripcion == None:
 		assert True
 	else:
 		rv = client.delete('/inscripciones/'+str(inscripcion.id))
-		assert True
+		if rv._status_code == 200:
+			assert True
+		else:
+			assert False
 
+def test_put_inscripcion_item(client):
+	inscripcion = Inscripcion.objects().first()
+	if inscripcion == None:
+		assert True
+	else:
+		data = {
+			'estado': 'estado',
+			'mensaje': 'mensaje'
+		}
+
+		rv = client.put('/inscripciones/'+str(inscripcion.id), data=data)
+		if rv._status_code == 200:
+			assert True
+		else:
+			assert False
+
+def test_post_inscripciones_aceptar(client):
+	recurso = Curso.objects().first()
+	if recurso == None:
+		assert True
+	else:
+		rv = client.post('/aceptar/inscripciones/recurso/'+str(recurso.id))
+		if rv._status_code == 200:
+			assert True
+		else:
+			assert False
+
+def test_get_inscripcion_recurso(client):
+	recurso = Curso.objects().first()
+	if recurso == None:
+		assert True
+	else:
+		rv = client.get('/inscripciones/recurso/'+str(recurso.id))
+		if rv._status_code == 200:
+			assert True
+		else:
+			assert False
+
+def test_post_inscripcion_recurso(client):
+	recurso = Curso.objects().first()
+	alumno = Alumno.objects().first()
+	if recurso == None or alumno == None:
+		assert True
+	else:
+		data = {
+			'id_alumno': alumno.id
+		}
+		data = json.dumps(data)
+		data = data.encode()
+		rv = client.post('/inscripciones/recurso/'+str(recurso.id), data=data)
+		if rv._status_code == 200:
+			assert True
+		else:
+			assert False
+
+def test_get_inscripciones_alumno(client):
+	inscripcion = Inscripcion.objects().first()
+	if inscripcion == None:
+		assert True
+	else:
+		rv = client.get('/inscripciones/alumno/'+str(inscripcion.id))
+		if rv._status_code == 200:
+			assert True
+		else:
+			assert False
+
+def test_get_inscripciones(client):
+	rv = client.get('/inscripciones')
+	if rv._status_code == 200:
+		assert True
+	else:
+		assert False
